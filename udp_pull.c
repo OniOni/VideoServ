498358231e91ec4dc883c72aff273040f19c8762
mathieu <sabourin.mathieu@free.fr>
2011-03-26 16:43:49 +0100

    Changed send_image_udp so that fragment + header are sent correctly.
    NOT TESTED.

--- udp_pull.c
+++ udp_pull.c
@@ -21,23 +21,33 @@ int send_image_udp(int sock, int image, int frag_size)
   int len_ima, len, sent, read, start;
   char str[12], *buff_ima;
   sprintf(str, "%d.jpg", image);
+
   file_to_buffer(str, &buff_ima, &len_ima);
 
   char * buff;
 
   do
   {
+    //get next fragment
     read = get_fragment(buff_ima, len_ima, start, start+frag_size, &buff);
     
-    sprintf(buff, "%d\r\n%d\r\n%d\r\s%s", image, len_ima, pos_pack, read,
-	    frag);
+    //build "header"
+    sprintf(buff, "%d\r\n%d\r\n%d\r\s", image, len_ima, pos_pack, read);
     perror("sprintf");
-
-
+      
     sent = len = strlen(buff);
+
+    //send "header"
     puts("Going to send image\n");
     do{
       sent -= send(sock, buff, len, 0);
+    }
+    while(sent > 0);
+    
+    sent = len = len_ima;
+    //send fragment
+    do{
+      sent -= send(sock, buff, read, 0);
     }
     while(sent > 0);
 
