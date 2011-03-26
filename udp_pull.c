2ce6397be7b1251555fe597de9430d9fc7e9e2e1
mathieu <sabourin.mathieu@free.fr>
2011-03-26 18:45:14 +0100

    Adapted read_init_udp from the tcp version.

--- udp_pull.c
+++ udp_pull.c
@@ -37,6 +37,95 @@ int get_fragment(char image[], int len, int start, int end, char ** frag)
   return read;
 }
 
+void read_init_udp(int sock, int * id, int * port_c, int * frag_size)
+{
+  int i, fini = 0, b_get = 1, b_end = 1;
+  char buff, buff_pre;
+  char * get = "GET ", * end = "END ";
+
+
+  *id = 0;
+  *port_c = 0;
+
+  for(i = 0; i < 4; i++)
+  {
+    do{}
+    while(recv(sock, &buff, 1, 0) != 1);
+      
+    //printf(":%c", buff);
+    if (buff != get[i])
+      b_get = 0;
+    if (buff != end[i])
+      b_end = 0;
+  }
+
+  //printf("get : %d  end : %d\n", b_get, b_end);
+
+  if(b_get)
+  {
+    //receive ID
+    fini = 0;
+    while(!fini)
+    {
+      do{}
+      while(recv(sock, &buff, 1, 0) != 1);
+      if (buff == ' ' || buff == '\n' || buff == '\r')
+	fini = 1;
+      else
+	*id = (*id * 10) + atoi(&buff); 	
+    }
+    printf("id %d\n", *id);
+    
+    fini = 0;
+    while(!fini)
+    {
+      do{}
+      while(recv(sock, &buff, 1, 0) != 1);
+      if (buff == ' ')
+	fini = 1;
+    }
+    puts("espace\n");
+  
+    //Receive listening port
+    fini = 0;
+    while(!fini)
+    {
+      do{}
+      while(recv(sock, &buff, 1, 0) != 1);
+      if (buff == ' ' || buff == '\n' || buff == '\r')
+	fini = 1;
+      else
+	*port_c = (*port_c * 10) + atoi(&buff); 	
+    }
+    printf("port : %d\n", *port_c);
+
+    fini = 0;
+    while(!fini)
+    {
+      do{}
+      while(recv(sock, &buff, 1, 0) != 1);
+      if (buff == ' ' || buff == '\n' || buff == '\r')
+	fini = 1;
+      else
+	*frag_size = (*frag_size * 10) + atoi(&buff); 	
+    }
+    printf("frag_size : %d\n", *port_c);
+  }
+  else
+  {
+    close(sock);
+  }
+  int nb_nl = 0;
+  while(nb_nl < 2)
+  {
+    do{}
+    while(recv(sock, &buff, 1, 0) != 1);
+    
+    if (buff == '\n')
+      nb_nl++;
+  }
+}
+
 int send_image_udp(int sock, int image, int frag_size)
 {
   int len_ima, len, sent, read, start, pos_pack;
@@ -79,9 +168,11 @@ int send_image_udp(int sock, int image, int frag_size)
   while(read == frag_size);
 }
 
+
+
 /*void udp_pull(int port, char * file)
 {
-  int sock = mk_sock(port, "127.0.0.1", SOCK_DGRAM | SOCK_NONBLOCK);
+  int sock = mk_sock_udp(port, "127.0.0.1");
 
   int id, c_port;
   struct epoll_event ev, events[MAX_EVENTS];
@@ -119,73 +210,7 @@ int send_image_udp(int sock, int image, int frag_size)
     int n;
     for (n = 0; n < nfds; ++n) {
       if (events[n].data.fd == sock) {
-	socklen_t size_addr = sizeof(struct sockaddr_in);
-	int csock = accept(sock, (struct sockaddr *)&saddr_client, &size_addr);
-	printf("accept : %s\n", strerror(errno));
-
-	printf("Connection de %s :: %d\n", inet_ntoa(saddr_client.sin_addr), 
-	       htons(saddr_client.sin_port));
-	if (csock == -1) {
-	  perror("accept");
-	  exit(EXIT_FAILURE);
-	}
-
-	ev.events = EPOLLIN | EPOLLET;
-	ev.data.fd = csock;
-	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, csock,
-		      &ev) == -1) {
-	  perror("epoll_ctl: csock");
-	  exit(EXIT_FAILURE);
-	}
-      }
-      else{
-	printf("envent type %d\n", events[n].events);
-	if (events[n].events == 1)
-	{
-	  struct sockaddr_in addr;
-	  int len;
-
-	  if(connected_clients[events[n].data.fd].data_socket == 0)
-	  {
-	    puts("Initialisation of data_socket");
-	    read_init(events[n].data.fd, &id, &c_port);
-	    printf("%d::%d\n", id, c_port);
-	    getsockname(events[n].data.fd, (struct sockaddr*)&addr, &len); 
-	    connected_clients[events[n].data.fd].data_socket = 
-	      connect_to(inet_ntoa(addr.sin_addr), c_port);
-	    connected_clients[events[n].data.fd].num_image = 0;
-	  }
-	  else
-	  {
-	    int id = 0;
-	    printf("Getting image number\n");
-	    read_get(events[n].data.fd, &id);
-	    printf("Image number %d\n", id);
-	    if (id > 0)
-	    {	      
-	      id = (id % nombre_image);
-	      printf("id %d\n", id);
-	      send_image_tcp(connected_clients[events[n].data.fd].data_socket, id);
-	      connected_clients[events[n].data.fd].num_image = id;
-	    }
-	    else if (id == -1)
-	    {
-	      id = connected_clients[events[n].data.fd].num_image + 1;
-	      id = (id % (nombre_image + 1));
-	      if (id == 0) id = 1;
-	      printf("id %d\n", id);
-	      send_image_tcp(connected_clients[events[n].data.fd].data_socket, id);
-	      connected_clients[events[n].data.fd].num_image = id;
-	    }
-	    else if (id == -42)
-	    {
-	      //do closing stuff here
-	      close(connected_clients[events[n].data.fd].data_socket);
-	      close(events[n].data.fd);
-	      connected_clients[events[n].data.fd].data_socket = 0;
-	      connected_clients[events[n].data.fd].num_image = 0;
-	      puts("closed connection");
-	    }
+	
 	    printf("Sent image %d\n", connected_clients[events[n].data.fd].num_image);
 	  }
 	}
