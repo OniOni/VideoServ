703ef025f4fa3355cbe1a871a0954be8f7146bdb
mathieu <sabourin.mathieu@free.fr>
2011-03-31 18:50:02 +0200

    Moved functions usable by udp_push and udp_pull to udp_utils

---
+++ udp_utils.h
@@ -0,0 +1,19 @@
+#ifndef UDP_UTILS
+#define UDP_UTILS
+
+struct udp_info{
+  int data_sock;
+  int num_image;
+  int frag_size;
+  int listen_port;
+  struct in_addr ip;
+  int start;
+};
+
+int get_fragment(char image[], int len, int start, int size, char ** frag, char ** all);
+
+void read_init_udp(int sock, int * id, int * port_c, int * frag_size);
+
+int send_image_udp(int sock, struct sockaddr_in dest, int image, int frag_size);
+
+#endif
